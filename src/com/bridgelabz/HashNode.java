50456053559fb09c577ce086eea0501db03b9be7
drisyaathul <drisyak809@gmail.com>
2023-05-01 19:58:19 +0800

    Added: Frequency of words in a sentence

---
+++ src/com/bridgelabz/HashNode.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class HashNode<K,V> {
+    /*
+      Initializing Data and Node using Generics
+    */
+    K key;
+    V value;
+    HashNode<K,V> next;
+
+    public HashNode(K key, V value) {
+        /*
+         Parameter Constructor
+        */
+        this.key = key;
+        this.value = value;
+    }
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node: " + " K = ").append(key).append(", V = ").append(value).append("\n");
+        if (next != null)
+            nodeString.append(" , ").append(next);
+        return nodeString.toString();
+    }
+}
